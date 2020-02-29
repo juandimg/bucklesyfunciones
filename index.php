8282bb81c3bae0060ada79909463ed72557bbc37
juandimg <61071152+juandimg@users.noreply.github.com>
2020-02-28 20:55:58 -0500

    ejemplo bucles y funciones

--- index.php
+++ index.php
@@ -17,4 +17,43 @@ echo("<br> ...............");
 $arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"James",'usuario4'=>"sandra"); 
 echo("<br>");
 print_r($arreglo);
+
+echo("<br> ...............");
+
+//tamaño de arreglo
+$tamano=count($arreglo);
+echo("<br>");
+echo("El tamaño de nuestro arreglo es: ". $tamano);
+echo("<br> ...............");
+
+//Bucle for
+for($centinela=0;$centinela<=10;$centinela++){
+    echo("<br>El valor de centinela es: ".$centinela."<br>");
+}
+//Bucle for para recorrer arreglos
+
+echo("<br> ...............");
+$arreglo=array("Juan","Maria","James","Sandra");
+for($centinela=0;$centinela<count($arreglo);$centinela++){
+echo("<br>".$arreglo[$centinela]."<br>");
+}
+
+//Bucle foreach para recorrer arreglos
+
+echo("<br> ...............");
+$arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"James",'usuario4'=>"sandra");
+foreach($arreglo as $clave=>$valor){
+    echo "<br>"."La clave es: ".$clave;
+    echo "<br>".($valor)."<br>";
+    echo("<br>");
+
+} 
+
+//bucle foreach paraarreglo indexado
+echo("<br> ...............");
+$arreglo=array("Juan","Maria","James","Sandra");
+foreach($arreglo as  $clave=>$valor){
+    echo "<br> El valor del cajon es: ".$clave;
+    echo "<br> El valor del cajon es: ".$valor;
+}
 ?>
